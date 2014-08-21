daede001815daae7e12783aad826e8e71c88b12b
Bojan Zivanovic <bojanz@gmail.com>
2014-08-21 23:40:33 +0200

    Complete the language generation script, add the generated data.

--- scripts/language/generate.php
+++ scripts/language/generate.php
@@ -2,6 +2,10 @@
 
 /**
  * Generates the yml files stored in resources/language.
+ *
+ * CLDR lists about 515 languages, many of them dead (like Latin or Old English).
+ * In order to decrease the list to a reasonable size, only the languages
+ * for which CLDR itself has translations are listed.
  */
 
 set_time_limit(0);
@@ -18,35 +22,54 @@ if (!file_exists($enLanguages)) {
     die("The $enLanguages file was not found");
 }
 
+// Locales listed without a "-" match all variants.
+// Locales listed with a "-" match only those exact ones.
+$ignoredLocales = array(
+    // Interlingua is a made up language.
+    'ia',
+    // Those locales are 90% untranslated.
+    'aa', 'as', 'az-Cyrl', 'az-Cyrl-AZ', 'bem', 'dua', 'gv', 'haw', 'ig', 'ii',
+    'kkj', 'kok', 'kw', 'lkt', 'mgo', 'nnh', 'nr', 'nso', 'om', 'os', 'pa-Arab',
+    'pa-Arab-PK', 'rw', 'sah', 'ss', 'ssy', 'st', 'tg', 'tn', 'ts', 'uz-Arab',
+    'uz-Arab-AF', 've', 'vo', 'xh',
+    // Special "grouping" locales.
+    'root', 'en-US-POSIX', 'en-001', 'en-150',
+);
+
 $languages = array();
 // Load the "en" data first so that it can be used as a fallback for
 // untranslated language names in other locales.
 $languageData = json_decode(file_get_contents($enLanguages), TRUE);
 $languageData = $languageData['main']['en']['localeDisplayNames']['languages'];
 foreach ($languageData as $languageCode => $languageName) {
-    if (in_array($languageCode, array('und', 'zxx'))) {
-        // Ignore "Unknown language" and "No linguistic content"
-        continue;
+    if (strpos($languageCode, '-alt-') === FALSE) {
+        $languages['en'][$languageCode] = array(
+            'code' => $languageCode,
+            'name' => $languageName,
+        );
     }
-    if (strpos($languageCode, '-alt-') !== FALSE) {
-        // Ignore alternative names.
-        continue;
-    }
-    $languages['en'][$languageCode] = array(
-        'code' => $languageCode,
-        'name' => $languageName,
-    );
 }
 
 // Gather available locales.
 $locales = array();
 if ($handle = opendir('../json_full/main')) {
     while (false !== ($entry = readdir($handle))) {
-        if (substr($entry, 0, 1) != '.' && !in_array($entry, array('en-001', 'en-150'))) {
-          $locales[] = $entry;
+        if (substr($entry, 0, 1) != '.') {
+            $entryParts = explode('-', $entry);
+            if (!in_array($entry, $ignoredLocales) && !in_array($entryParts[0], $ignoredLocales)) {
+                $locales[] = $entry;
+            }
         }
     }
     closedir($handle);
+}
+
+// Remove all languages that aren't an available locale at the same time.
+// This reduces the language list from about 515 to about 185 languages.
+foreach ($languages['en'] as $languageCode => $languageData) {
+    if (!in_array($languageCode, $locales)) {
+        unset($languages['en'][$languageCode]);
+    }
 }
 
 // Load the localizations.
@@ -54,24 +77,17 @@ foreach ($locales as $locale) {
     $data = json_decode(file_get_contents('../json_full/main/' . $locale . '/languages.json'), TRUE);
     $data = $data['main'][$locale]['localeDisplayNames']['languages'];
     foreach ($data as $languageCode => $languageName) {
-        if (in_array($languageCode, array('und', 'zxx'))) {
-            // Ignore "Unknown language" and "No linguistic content"
-            continue;
-        }
-        if (strpos($languageCode, '-alt-') !== FALSE) {
-            // Ignore alternative names.
-            continue;
-        }
+        if (isset($languages['en'][$languageCode])) {
+            // This language name is untranslated, use to the english version.
+            if ($languageCode == $languageName) {
+                $languageName = $languages['en'][$languageCode]['name'];
+            }
 
-        // This language name is untranslated, use to the english version.
-        if ($languageCode == $languageName) {
-            $languageName = $languages['en'][$languageCode]['name'];
+            $languages[$locale][$languageCode] = array(
+                'code' => $languageCode,
+                'name' => $languageName,
+            );
         }
-
-        $languages[$locale][$languageCode] = array(
-            'code' => $languageCode,
-            'name' => $languageName,
-        );
     }
 }
 
