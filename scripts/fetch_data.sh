fac94c042f0c890d694c136c06f10716eb32478b
Hector del Rio <hrc.delrio@gmail.com>
2015-08-24 11:37:43 +0300

    fetch_data.sh URL updated. It can also use cURL now.

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
+
+URL="http://www.currency-iso.org/dam/downloads/lists/list_one.xml"
+
+rm -fR assets
 mkdir assets
 cd assets
+
 git clone https://github.com/unicode-cldr/cldr-core.git
 git clone https://github.com/unicode-cldr/cldr-numbers-full.git
 git clone https://github.com/unicode-cldr/cldr-localenames-full.git
-wget http://www.currency-iso.org/dam/downloads/table_a1.xml -O c2.xml
+
+if command -v wget >/dev/null 2>&1;
+then
+    wget $URL -O c2.xml
+else 
+    if command -v curl >/dev/null 2>&1;
+    then
+        curl $URL > c2.xml
+    else
+        echo "I require wget or curl but it's not installed.  Aborting."
+        exit 1
+    fi
+fi
